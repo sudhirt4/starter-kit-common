c68c33fdeeb41756f923ed044a287fb858fb7b42
Sudhir Shrestha <sudhirshrestha@live.com>
2018-02-06 13:15:01 +0545

    Validation changes

---
+++ schemas/auth.js
@@ -0,0 +1,14 @@
+import Joi from "joi";
+
+export const login = {
+  user: {
+    email: Joi.string()
+      .email()
+      .max(50)
+      .required(),
+    password: Joi.required()
+  },
+  test: Joi.array().items({
+    x: Joi.string().required()
+  })
+};
